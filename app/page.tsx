d34d3fdf31743628a946be68480f4ea2daf759c9
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-07-07 20:38:40 +0530

    feat: add basic timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Timeline from "@/components/Timeline/index";
+
 export default function Home() {
   return (
-    <></>
+    <div className="background-bg min-h-screen flex flex-col grow">
+      <Timeline />
+    </div>
   );
 }
