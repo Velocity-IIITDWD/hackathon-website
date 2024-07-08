731a43c76f37eecd5a8d9d54fe25b8d224faa19f
AyushSingh916 <ayushsingh916924@gmail.com>
2024-07-08 21:07:11 +0530

    adding animation

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ const AboutUs: React.FC = () => {
     <div className="relative flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-900 to-black text-white overflow-hidden">
       <h1 className="text-6xl font-bold mt-16">About Us</h1>
       <div className="relative mt-8 w-full overflow-hidden">
-        <div className="flex animate-move-cards space-x-0"> 
+        <div className="flex animate-move-cards space-x--1"> 
           {Array.from({ length: 11 }).map((_, index) => (
             <div
               key={index}
@@ -26,7 +26,9 @@ const AboutUs: React.FC = () => {
           ))}
         </div>
       </div>
-      <div className="relative mt-16 max-w-2xl p-8 bg-gray-700 bg-opacity-75 rounded shadow-lg">
+      <div className="relative mt-16 max-w-2xl p-8 bg-gray-700 bg-opacity-75 rounded-lg shadow-lg">
+        <div className="corner-decorator corner-top-left"></div>
+        <div className="corner-decorator corner-bottom-right"></div>
         <p className="text-center text-lg">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
           standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
