ee30abb06a59eea04339f6caa80423e1cc9cd014
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-07-09 17:27:53 +0530

    feat: animated sponsors logo

--- components/SponsorCard.tsx
+++ components/SponsorCard.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+'use client'
+
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube, faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 interface SponsorCardProps {
   name: string;
@@ -15,11 +20,33 @@ interface SponsorCardProps {
 }
 
 const SponsorCard: React.FC<SponsorCardProps> = ({ name, website, logo, socialLinks }) => {
+  const [hovered, setHovered] = useState(false);
+  const logoRef1 = useRef(null);
+  const logoRef2 = useRef(null);
+  const logoRef3 = useRef(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({defaults: {duration: 0.5, stagger: 0}});
+
+    if (hovered) {
+      gsap.to(logoRef1.current, { xPercent: -15, yPercent: -15, ease: "power1.inOut" })
+      gsap.to(logoRef2.current, { xPercent: -10, yPercent: -10, ease: "power1.inOut" })
+      gsap.to(logoRef3.current, { xPercent: -5, yPercent: -5, ease: "power1.inOut" })
+    } else {
+      gsap.to(logoRef1.current, { xPercent: 0, yPercent: 0, ease: "power2.inOut" })
+      gsap.to(logoRef2.current, { xPercent: 0, yPercent: 0, ease: "power2.inOut" })
+      gsap.to(logoRef3.current, { xPercent: 0, yPercent: 0, ease: "power2.inOut" })
+    }
+  }, [hovered])
+
   return (
-    <div className="w-80 bg-gradient-to-br from-gray-900 via-black to-gray-900 border border-gray-700 rounded-lg shadow-xl p-6 flex flex-col justify-between transform transition duration-500 hover:scale-105">
+    <div className="w-80 bg-gradient-to-br from-gray-900 via-black to-gray-900 border border-gray-700 rounded-lg shadow-xl p-6 flex flex-col justify-between transform transition duration-500">
       <div className="flex items-center mb-6">
-        <div className="w-16 h-16 flex justify-center items-center bg-gradient-to-r from-blue-600 to-purple-600 rounded-full border-2 border-gray-300 shadow-md overflow-hidden">
-          <img src={logo} alt={`${name} logo`} className="w-full h-full object-cover" />
+        <div onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)} className="w-16 h-16 flex justify-center items-center rounded-full shadow-md relative">
+          <Image ref={logoRef1} height={50} width={50} src={logo} alt={`${name} logo`} className="opacity-100 z-30 w-full h-full object-cover absolute top-0 left-0 rounded-full" />
+          <Image ref={logoRef2} height={50} width={50} src={logo} alt={`${name} logo`} className="opacity-80 z-20 w-full h-full object-cover absolute top-0 left-0 rounded-full" />
+          <Image ref={logoRef3} height={50} width={50} src={logo} alt={`${name} logo`} className="opacity-60 z-10 w-full h-full object-cover absolute top-0 left-0 rounded-full" />
+          <Image height={50} width={50} src={logo} alt={`${name} logo`} className="opacity-40 w-full h-full object-cover absolute top-0 left-0 rounded-full" />
         </div>
         <div className="ml-4">
           <h2 className="text-white text-2xl font-semibold">{name}</h2>
