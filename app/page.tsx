6e708ff76fe3ddc115437a52ed5482fe9c24d4b2
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-07-11 19:20:36 +0530

    feat: added server action for isMobileDevice; feat: Updated Desktop View Code for timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import { isMobileDevice } from "@/lib/responsive";
+
 import About from "@/components/About";
 import Timeline from "@/components/Timeline/index";
 
-export default function Home() {
+export default async function Home() {
+  const mobile = await isMobileDevice();
   return (
     <div className="-z-10 background-bg flex flex-col gap-10">
       <About />
-      <Timeline />
+      <Timeline isMobile={mobile} />
     </div>
   );
 }
