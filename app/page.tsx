465df6c61c5190529a7711739fa6264f33d8cd72
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-07-11 23:08:52 +0530

    Merge Landing into main (squash)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { isMobileDevice } from "@/lib/responsive";
 
 import About from "@/components/About";
+import Landing from "@/components/Landing/Landing";
+import Navbar from "@/components/Landing/Navbar";
 import Timeline from "@/components/Timeline";
 import Carousel from "@/components/carousel/Carousel";
 import Sponsors from "@/components/Sponsors";
@@ -9,6 +11,8 @@ export default async function Home() {
   const mobile = await isMobileDevice();
   return (
     <div className="-z-10 background-bg flex flex-col gap-10">
+      <Navbar />
+      <Landing />
       <About />
       <Carousel />
       <Timeline isMobile={mobile} />
