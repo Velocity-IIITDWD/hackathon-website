c53978bda5a0afc7cc309621d4974b685621cb65
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-07-15 18:27:05 +0530

    fix: loader improved

--- app/loading/index.tsx
+++ app/loading/index.tsx
@@ -13,7 +13,7 @@ function Loader({ rendering }: { rendering?: boolean }) {
   useGSAP(() => {
     const img1 = document.getElementById("loader-img1");
     const img2 = document.getElementById("loader-img2");
-    const loadingScreen = document.getElementById("loader");
+    const loadingScreen = document.querySelectorAll(".loader");
     gsap.set(img1, {
       x: "-480px",
     });
@@ -37,25 +37,27 @@ function Loader({ rendering }: { rendering?: boolean }) {
       repeat: -1,
     });
     if (rendering) {
-      gsap.to(loadingScreen, {
-        y: "-100%",
-        delay: 5,
-        duration: 1.5,
-        ease: "sine.inOut"
+      loadingScreen.forEach(el => {
+        gsap.to(el, {
+          y: "-100%",
+          delay: 4.5,
+          duration: 1,
+          ease: "power1.in"
+        })
       })
     }
   });
 
   return (
-    <div id="loader" className="z-40 loading-background fixed">
-      <div className="z-50 fixed h-screen w-screen backdrop-blur-3xl grid place-items-center">
+    <div className="loader z-40 loading-background fixed">
+      <div className="loader z-50 fixed h-screen w-screen backdrop-blur-3xl grid place-items-center">
         <Image src={Stars} alt="stars bg" className="-z-10 absolute" />
         <div className="rounded-full size-40 loading-bg relative overflow-hidden">
           <Image id="loader-img1" src={Flow1} alt="landing bg" className="z-10 absolute bottom-0 min-w-[700px] translate-y-[60%]" />
           <Image id="loader-img2" src={Flow2} alt="landing bg" className="z-0 absolute bottom-0 min-w-[700px] translate-y-[60%]" />
         </div>
       </div>
-      <div className="z-40 fixed w-screen h-screen loading-background background-bg">
+      <div className="loader z-40 fixed w-screen h-screen loading-background background-bg">
         <div className="w-60 h-10 blur-3xl bg-gradient-to-r from-[#5200FF] to-[#921BBC] translate-x-[20vw] translate-y-[15vh]" />
         <div className="w-60 h-10 blur-3xl bg-gradient-to-r from-[#5200FF] to-[#921BBC] translate-x-[75vw] translate-y-[80vh]" />
         <div className="w-60 h-10 blur-3xl bg-gradient-to-r from-[#5200FF] to-[#921BBC] translate-x-[80vw] translate-y-[5vh]" />
@@ -72,7 +74,7 @@ export default function Page({ rendering = false }: { rendering?: boolean }) {
     if (rendering) {
       const timer = setTimeout(() => {
         setShowLoader(false);
-      }, 6600);
+      }, 7000);
       return () => clearTimeout(timer);
     }
   }, [rendering]);
