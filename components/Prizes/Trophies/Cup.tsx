0cdd0298a0b4a099b0e61c253e2fe9026a7fe4d4
palepen <122384892+palepen@users.noreply.github.com>
2024-07-26 18:36:56 +0530

    fix(Prizes): fixed some screen height issues

--- components/Prizes/Trophies/Cup.tsx
+++ components/Prizes/Trophies/Cup.tsx
@@ -9,13 +9,13 @@ const Cup = ({ prize, cupImage, cupColor, className }: CupProps) => {
   return (
     <div
       className={
-        "relative w-4/5 sm:w-3/5 md:w-[30%] lg:w-1/4 xl:w-1/5 py-2 flex flex-col items-center justify-center cup-div before:transition before:duration-300 before:ease-in before:delay-75 hover:before:opacity-70 border-l-2 border-r-2 group " +
+        "relative w-[70%] sm:w-3/5 md:w-[30%] lg:w-1/4 xl:w-[17%] py-2 flex flex-col items-center justify-center cup-div before:transition before:duration-300 before:ease-in before:delay-75 hover:before:opacity-70 border-l-2 border-r-2 group " +
         className
       }
     >
       <div
         className={
-          "w-full bg-inherit p-4 cup-container grid-lines-bg flex flex-col bg-gradient-carousel items-center justify-center text-2xl md:text-xl lg:text-2xl 2xl:text-3xl " +
+          "w-full h-full bg-inherit p-4 cup-container grid-lines-bg flex flex-col bg-gradient-carousel items-center justify-center text-2xl md:text-xl lg:text-2xl 2xl:text-3xl " +
           `${cupColor}`
         }
       >
--- components/Prizes/Trophies/Trophies.tsx
+++ components/Prizes/Trophies/Trophies.tsx
@@ -4,11 +4,11 @@ import Cup from "@/components/Prizes/Trophies/Cup";
 const Trophies = () => {
   return (
     <>
-      <div className="hidden md:flex p-4 gap-8 w-full h-fit items-center justify-evenly z-10">
+      <div className="hidden md:flex p-2 gap-4 w-full h-fit items-center justify-evenly z-10">
         <Cup
           cupImage={"/SilverCup.svg"}
           cupColor={"silver"}
-          className={"mt-24"}
+          className={"mt-16"}
           prize={"2nd Prize"}
         />
         <Cup
@@ -20,15 +20,15 @@ const Trophies = () => {
         <Cup
           cupImage={"/BronzeCup.png"}
           cupColor={"bronze"}
-          className={"mt-48"}
+          className={"mt-32"}
           prize={"3rd Prize"}
         />
       </div>
-      <div className="flex flex-col md:hidden p-8 gap-16 w-full h-fit items-center justify-evenly">
+      <div className="flex flex-col md:hidden p-4 gap-8 w-full h-fit items-center justify-start">
         <Cup
           cupImage={"/GoldenCup.svg"}
           cupColor={"gold"}
-          className={"mt-24 group/gold"}
+          className={"mt-6 group/gold"}
           prize={"1st Prize"}
         />
         <Cup
