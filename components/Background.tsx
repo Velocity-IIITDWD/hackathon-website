daefd08c99a6fb2db6205f492e3d8ed5a787024a
palepen <122384892+palepen@users.noreply.github.com>
2024-07-27 22:32:17 +0530

    feat(Background.tsx): added bacground Hazes

---
+++ components/Background.tsx
@@ -0,0 +1,44 @@
+const Background = () => {
+  return (
+    <div className="absolute flex opacity-15 blur-3xl flex-col z-[5] w-full h-full overflow-clip">
+      <div className="relative top-[20%] bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="flex flex-col md:flex-row w-full overflow-clip h-[10%] rotate-12">
+        <div className="bg-[#FF9900] opacity-55 w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#304094] w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#FF9900] opacity-55  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+      </div>
+      <div className="bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#054325]  self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#B745BA] overflow-clip  self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="flex flex-col md:flex-row w-full h-[10%] -rotate-45">
+        <div className="bg-[#054325] w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#304094] w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#FF9900] opacity-55  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+      </div>
+      <div className="bg-[#054325] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#FF9900] opacity-55 self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#B745BA] self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="flex flex-col md:flex-row w-full overflow-clip h-[10%] rotate-12">
+        <div className="bg-[#054325] w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#304094] w-1/2 md:w-1/4 h-[50%] rounded-full" />
+        <div className="bg-[#FF9900] opacity-55  self-end w-1/2 md:w-1/4 h-[50%] rounded-full" />
+      </div>
+      <div className="bg-[#054325] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] w-1/2 md:w-1/4 self-end h-[4%] rounded-full" />
+      <div className="bg-[#FF9900] opacity-55 opacity-55  w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#304094] w-1/2 md:w-1/4 h-[4%] rounded-full" />
+      <div className="bg-[#B745BA]  self-end w-1/2 md:w-1/4 h-[4%] rounded-full" />
+    </div>
+  );
+};
+
+export default Background;
