6481a3d06cdca62d9bd286114343bee830246a73
Harsh Soni <52278293+harshsoni-harsh@users.noreply.github.com>
2024-10-02 11:06:13 +0530

    team page updated

--- components/Team/TeamData.tsx
+++ components/Team/TeamData.tsx
@@ -13,6 +13,34 @@ interface Team {
 
 const teamData: Team[] = [
   {
+    name: "Management Team",
+    members: [
+      {
+        name: "S G Sithartha",
+        image: "/team/S_G_Sithartha.webp",
+        title: "Member",
+        linkedIn:
+            "https://www.linkedin.com/in/sitharthasg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "",
+      },
+      {
+        name: "R S Gokul Varun",
+        image: "/team/R_S_Gokul_Varun.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Jaishana Bindhu Priya",
+        image: "/team/Jaishana_Bindhu_Priya.webp",
+        title: "Lead",
+        linkedIn:
+            "https://www.linkedin.com/in/jaishana-bindhu-priya?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+        github: "https://github.com/jaishana25/jaishana25",
+      },
+    ],
+  },
+  {
     name: "Sponsorship Team",
     members: [
       {
@@ -24,8 +52,8 @@ const teamData: Team[] = [
         github: "https://github.com/void-delta",
       },
       {
-        name: "Dhanraj",
-        image: "/team/dummy.webp",
+        name: "Dhanraj Matke",
+        image: "/team/Dhanraj_Matke.webp",
         title: "Member",
         linkedIn: "",
         github: "",
@@ -47,7 +75,7 @@ const teamData: Team[] = [
       {
         name: "Srijan Raghavula",
         image: "/team/Srijan_Raghavula.webp",
-        title: "Backend Developer",
+        title: "Member",
         linkedIn: "",
         github: "",
       },
@@ -82,150 +110,6 @@ const teamData: Team[] = [
     ],
   },
   {
-    name: "Technical Team",
-    members: [
-      {
-        name: "Chinmayee Patankar",
-        image: "/team/Chinmayee_Patankar.webp",
-        title: "Member",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "Karthik Avinash",
-        image: "/team/Karthik_Avinash.webp",
-        title: "Member",
-        linkedIn: "https://www.linkedin.com/in/karthik-avinash-04b51022b/",
-        github: "",
-      },
-      {
-        name: "Harsh Soni",
-        image: "/team/Harsh_Soni.webp",
-        title: "Member",
-        linkedIn: "https://www.linkedin.com/in/harsh-soni-/",
-        github: "https://github.com/harshsoni-harsh/",
-      },
-      {
-        name: "Azmi Nabeel",
-        image: "/team/Azmi_Nabeel.webp",
-        title: "Member",
-        linkedIn: "https://www.linkedin.com/in/azmi-nabeel",
-        github: "https://github.com/azmi-nabeel",
-      },
-      {
-        name: "Aditya Vikram Singh",
-        image: "/team/Aditya_Vikram_Singh.webp",
-        title: "Member",
-        linkedIn:
-          "https://www.linkedin.com/in/aditya-vikram-singhgh-0a260a266/",
-        github: "https://github.com/Adityav2003",
-      },
-    ],
-  },
-  {
-    name: "Designing Team",
-    members: [
-      {
-        name: "Arsalan Shaikh",
-        image: "/team/Shaikh_Mohammed_Arsalan.webp",
-        title: "UI/UX DESIGNER",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "G Rohith Yadav",
-        image: "/team/G_Rohith_Yadav.webp",
-        title: "Member",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "Lakshmi Prasad",
-        image: "/team/lakshmi_prasad.webp",
-        title: "Member",
-        linkedIn: "https://www.linkedin.com/in/lakshmi-prasad-doddi-26a30029a/",
-        github: "",
-      },
-      {
-        name: "S Nomtha Prakash",
-        image: "/team/S_Nomtha_Prakash.webp",
-        title: "Member",
-        linkedIn: "linkedin.com/in/nomtha-prakash-330529288",
-        github: "",
-      },
-      {
-        name: "Karthikeya",
-        image: "/team/Karthikeya.webp",
-        title: "Member",
-        linkedIn:
-          "https://www.linkedin.com/in/m-karthikeya-b7751a2a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: "",
-      },
-      {
-        name: "Pj Vineeth Kumar",
-        image: "/team/Pj_Vineeth_Kumar.webp",
-        title: "UI/UX Designer",
-        linkedIn:
-          "https://www.linkedin.com/in/pj-vineeth-kumar-88610927b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: "",
-      },
-      {
-        name: "Syed Mahdee Husain",
-        image: "/team/Syed_Mahdee_Husain.webp",
-        title: "Member",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "Ngamchingseh Willis Kipgen",
-        image: "/team/Ngamchingseh_Willis_Kipgen.webp",
-        title: "Member",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "Hardik Mittal",
-        image: "/team/Hardik_Mittal.webp",
-        title: "Member",
-        linkedIn: "https://www.linkedin.com/in/hardikm21",
-        github: "",
-      },
-    ],
-  },
-  {
-    name: "Velocity Team",
-    members: [
-      {
-        name: "Ayush Singh",
-        image: "/team/Ayush_Singh.webp",
-        title: "NextJs Developer",
-        linkedIn: "https://www.linkedin.com/in/ayush-singh-357b32257/",
-        github: "https://github.com/AyushSingh916",
-      },
-      {
-        name: "C. Nikhil Karthik",
-        image: "/team/C._Nikhil_Karthik.webp",
-        title: "Front end Developer",
-        linkedIn: "https://www.linkedin.com/in/nikhilkarthik24",
-        github: "https://github.com/C-NikhilKarthik",
-      },
-      {
-        name: "Harsh Soni",
-        image: "/team/Harsh_Soni.webp",
-        title: "Full Stack Developer",
-        linkedIn: "https://www.linkedin.com/in/harsh-soni-/",
-        github: "https://github.com/harshsoni-harsh/",
-      },
-      {
-        name: "Lupam Kumar Saha",
-        image: "/team/Lupam_Kumar_Saha.webp",
-        title: "Front End Developer",
-        linkedIn: "https://www.linkedin.com/in/lupamks",
-        github: "https://github.com/palepen",
-      },
-    ],
-  },
-  {
     name: "Event Management Team",
     members: [
       {
@@ -233,7 +117,7 @@ const teamData: Team[] = [
         image: "/team/S_G_Sithartha.webp",
         title: "Lead",
         linkedIn:
-          "https://www.linkedin.com/in/sitharthasg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+            "https://www.linkedin.com/in/sitharthasg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         github: "",
       },
       {
@@ -241,7 +125,7 @@ const teamData: Team[] = [
         image: "/team/Swaroop_Patil.webp",
         title: "Member",
         linkedIn:
-          "https://www.linkedin.com/in/swaroop-patil-77b616264?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+            "https://www.linkedin.com/in/swaroop-patil-77b616264?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
         github: "",
       },
       {
@@ -303,34 +187,6 @@ const teamData: Team[] = [
     ],
   },
   {
-    name: "Management Team",
-    members: [
-      {
-        name: "S G Sithartha",
-        image: "/team/S_G_Sithartha.webp",
-        title: "Member",
-        linkedIn:
-          "https://www.linkedin.com/in/sitharthasg?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: "",
-      },
-      {
-        name: "R S Gokul Varun",
-        image: "/team/R_S_Gokul_Varun.webp",
-        title: "Member",
-        linkedIn: "",
-        github: "",
-      },
-      {
-        name: "Jaishana Bindhu Priya",
-        image: "/team/Jaishana_Bindhu_Priya.webp",
-        title: "Lead",
-        linkedIn:
-          "https://www.linkedin.com/in/jaishana-bindhu-priya?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-        github: "https://github.com/jaishana25/jaishana25",
-      },
-    ],
-  },
-  {
     name: "Marketing & Outreach Team",
     members: [
       {
@@ -366,15 +222,15 @@ const teamData: Team[] = [
         image: "/team/Riti_Joshi.webp",
         title: "Member",
         linkedIn:
-          "https://www.linkedin.com/in/riti-joshi-594a47327?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+            "https://www.linkedin.com/in/riti-joshi-594a47327?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         github: "",
       },
       {
         name: "Namita Wayal",
         image: "/team/Namita_Wayal.webp",
-        title: "Lead",
+        title: "Co-lead",
         linkedIn:
-          "https://www.linkedin.com/in/namita-wayal-aa35ab324?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+            "https://www.linkedin.com/in/namita-wayal-aa35ab324?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
         github: "",
       },
       {
@@ -405,29 +261,132 @@ const teamData: Team[] = [
         linkedIn: "",
         github: "",
       },
+      {
+        name: "R S Gokul Varun",
+        image: "/team/R_S_Gokul_Varun.webp",
+        title: "Lead",
+        linkedIn: "",
+        github: "",
+      },
     ],
   },
   {
-    name: "Logistics",
+    name: "Technical Team",
     members: [
       {
-        name: "Nitish Naveen",
-        image: "/team/Nitish_Naveen.webp",
-        title: "Co-Lead Logistics",
-        linkedIn:
-          "https://www.linkedin.com/in/nitish-naveen-540433300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        github: "",
+        name: "Aayush Kumar",
+        image: "/team/Aayush_Kumar.webp",
+        title: "Lead",
+        linkedIn: "https://www.linkedin.com/in/aayush-kumar-904967218/",
+        github: "https://github.com/Aayush-Kr-13",
       },
       {
-        name: "Om Pandey",
-        image: "/team/Om_pandey.webp",
+        name: "Chinmayee Patankar",
+        image: "/team/Chinmayee_Patankar.webp",
         title: "Member",
         linkedIn: "",
         github: "",
       },
       {
-        name: "Saagarika Ural S",
-        image: "/team/Saagarika_Ural_S.webp",
+        name: "Karthik Avinash",
+        image: "/team/Karthik_Avinash.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/karthik-avinash-04b51022b/",
+        github: "",
+      },
+      {
+        name: "Harsh Soni",
+        image: "/team/Harsh_Soni.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/harsh-soni-/",
+        github: "https://github.com/harshsoni-harsh/",
+      },
+      {
+        name: "Azmi Nabeel",
+        image: "/team/Azmi_Nabeel.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/azmi-nabeel",
+        github: "https://github.com/azmi-nabeel",
+      },
+      {
+        name: "Aditya Vikram Singh",
+        image: "/team/Aditya_Vikram_Singh.webp",
+        title: "Member",
+        linkedIn:
+            "https://www.linkedin.com/in/aditya-vikram-singhgh-0a260a266/",
+        github: "https://github.com/Adityav2003",
+      },
+    ],
+  },
+  {
+    name: "Designing Team",
+    members: [
+      {
+        name: "Arsalan Shaikh",
+        image: "/team/Shaikh_Mohammed_Arsalan.webp",
+        title: "UI/UX DESIGNER",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "G Rohith Yadav",
+        image: "/team/G_Rohith_Yadav.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Lakshmi Prasad",
+        image: "/team/lakshmi_prasad.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/lakshmi-prasad-doddi-26a30029a/",
+        github: "",
+      },
+      {
+        name: "Karthikeya",
+        image: "/team/Karthikeya.webp",
+        title: "Member",
+        linkedIn:
+            "https://www.linkedin.com/in/m-karthikeya-b7751a2a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "",
+      },
+      {
+        name: "Pj Vineeth Kumar",
+        image: "/team/Pj_Vineeth_Kumar.webp",
+        title: "UI/UX Designer",
+        linkedIn:
+            "https://www.linkedin.com/in/pj-vineeth-kumar-88610927b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "",
+      },
+      {
+        name: "Ngamchingseh Willis Kipgen",
+        image: "/team/Ngamchingseh_Willis_Kipgen.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Hardik Mittal",
+        image: "/team/Hardik_Mittal.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/hardikm21",
+        github: "",
+      },
+      {
+        name: "Ekansh",
+        image: "/team/dummy.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+    ],
+  },
+  {
+    name: "Logistics Team",
+    members: [
+      {
+        name: "Chinmay Pramod Ardalkar ",
+        image: "/team/Chinmay_Pramod_Ardalkar.webp",
         title: "Member",
         linkedIn: "",
         github: "",
@@ -440,11 +399,87 @@ const teamData: Team[] = [
         github: "",
       },
       {
+        name: "Nirbhay Singh",
+        image: "/team/Nirbhay_Kumar.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
         name: "Charvi Mantri",
         image: "/team/Charvi_Mantri.webp",
         title: "Member",
         linkedIn: "",
         github: "",
+      },
+      {
+        name: "Priyanshu Mittal",
+        image: "/team/Priyanshu_Mittal.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Saagarika Ural S",
+        image: "/team/Saagarika_Ural_S.webp",
+        title: "Member",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Arnav Gupta",
+        image: "/team/Arnav_Gupta.webp",
+        title: "Member",
+        linkedIn: "https://www.linkedin.com/in/arnavguptas/",
+        github: "",
+      },
+      {
+        name: "Rishi Koushik",
+        image: "/team/dummy.webp",
+        title: "Lead",
+        linkedIn: "",
+        github: "",
+      },
+      {
+        name: "Nitish Naveen",
+        image: "/team/Nitish_Naveen.webp",
+        title: "Co-lead",
+        linkedIn:
+            "https://www.linkedin.com/in/nitish-naveen-540433300?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github: "",
+      }
+    ],
+  },
+  {
+    name: "Velocity Team",
+    members: [
+      {
+        name: "Ayush Singh",
+        image: "/team/Ayush_Singh.webp",
+        title: "NextJs Developer",
+        linkedIn: "https://www.linkedin.com/in/ayush-singh-357b32257/",
+        github: "https://github.com/AyushSingh916",
+      },
+      {
+        name: "C. Nikhil Karthik",
+        image: "/team/C._Nikhil_Karthik.webp",
+        title: "Front end Developer",
+        linkedIn: "https://www.linkedin.com/in/nikhilkarthik24",
+        github: "https://github.com/C-NikhilKarthik",
+      },
+      {
+        name: "Harsh Soni",
+        image: "/team/Harsh_Soni.webp",
+        title: "Full Stack Developer",
+        linkedIn: "https://www.linkedin.com/in/harsh-soni-/",
+        github: "https://github.com/harshsoni-harsh/",
+      },
+      {
+        name: "Lupam Kumar Saha",
+        image: "/team/Lupam_Kumar_Saha.webp",
+        title: "Front End Developer",
+        linkedIn: "https://www.linkedin.com/in/lupamks",
+        github: "https://github.com/palepen",
       },
     ],
   },
