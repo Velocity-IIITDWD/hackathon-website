75d7eb7aa96b707ce24de12a8d4158384d59c07e
AyushSingh916 <ayushsingh916924@gmail.com>
2024-10-16 22:26:36 +0530

    adding gold and silver prize

--- components/Prizes/Trophies/Trophies.tsx
+++ components/Prizes/Trophies/Trophies.tsx
@@ -10,22 +10,22 @@ const Trophies = () => {
           cupColor={'silver'}
           className={'mt-16'}
           prize={'2nd Prize'}
-          amount="25"
+          amount="50"
         />
         <Cup
           cupImage={'/GoldenCup.svg'}
           cupColor={'gold'}
-          className={'mt-0'}
+          className={'mt-16'}
           prize={'1st Prize'}
-          amount="40"
+          amount="75"
         />
-        <Cup
+        {/* <Cup
           cupImage={'/BronzeCup.png'}
           cupColor={'bronze'}
           className={'mt-32'}
           prize={'3rd Prize'}
-          amount="15"
-        />
+          amount="Not Decided"
+        /> */}
       </div>
       <div className="flex h-fit w-full flex-col items-center justify-start gap-8 p-4 md:hidden">
         <Cup
@@ -33,22 +33,22 @@ const Trophies = () => {
           cupColor={'gold'}
           className={'group/gold mt-6'}
           prize={'1st Prize'}
-          amount="40"
+          amount="75"
         />
         <Cup
           cupImage={'/SilverCup.svg'}
           cupColor={'silver'}
           className={'group/silver'}
           prize={'2nd Prize'}
-          amount="25"
+          amount="50"
         />
-        <Cup
+        {/* <Cup
           cupImage={'/BronzeCup.png'}
           cupColor={'bronze'}
           className={'group/bronze'}
           prize={'3rd Prize'}
           amount="15"
-        />
+        /> */}
       </div>
     </>
   );
